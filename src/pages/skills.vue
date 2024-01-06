843ef23363ead2d1e019da69f22515345dedcc56
thomas <tlasalmonie@gmail.com>
2024-01-06 08:49:09 -0500

    fix: Fix linter error

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
+  import type { Skill } from '~/utils/types/skills.types';
+  import useFetchData from '~/utils/useFetch';
+
+  const { result, isLoading, error, fetchData } = useFetchData<Skill[]>(`/api/skills/`);
+
+  const skillsList = computed(() => result.value?.filter((skill: Skill) => skill.show));
+
+  await fetchData();
 </script>
 
 <template>
-  <Construction />
+  <Html lang="en">
+    <Head>
+      <Title>My skills - Thomas La Salmonie</Title>
+      <Meta name="description" content="List of my skills that I develop along my carreer" />
+    </Head>
+  </Html>
+  <h1 class="text-center">My Skills</h1>
+  <AsyncLoader :is-loading="isLoading" :error="error">
+    <v-container>
+      <v-row justify="space-around">
+        <v-col v-for="skill in skillsList" :key="skill.key" cols="12" sm="6" md="4" lg="3" xl="2">
+          <SkillItem :skill="skill" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </AsyncLoader>
 </template>
--- src/utils/types/common.types.ts
+++ src/utils/types/common.types.ts
@@ -6,6 +6,7 @@ export type Nullable<T> = T | null;
 /** Makes an interface to also be undefined */
 export type Maybe<T> = T | undefined;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AnyObject<T = any> = Record<string, T>;
 export type UnknownObject = Record<string, unknown>;
 export type EmptyObject = Record<string, never>;
